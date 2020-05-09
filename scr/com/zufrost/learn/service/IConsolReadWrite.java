d5a2c9a66361a445a8bffd6ecbd688be914301d3
Alex Frost <zufrost.a@gmail.com>
2020-05-09 12:23:43 +0300

    make IConsolReadWrite interface

---
+++ scr/com/zufrost/learn/service/IConsolReadWrite.java
@@ -0,0 +1,12 @@
+package com.zufrost.learn.service;
+
+import com.zufrost.learn.entity.MathematicalExpression;
+
+public interface IConsolReadWrite {
+
+    MathematicalExpression read();
+
+//    Answer write (Answer answer); //todo
+
+
+}
