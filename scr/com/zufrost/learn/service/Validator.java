0296e376a6c1d2a2483a5009c055df0c6b9692a1
Alex Frost <zufrost.a@gmail.com>
2020-05-09 16:30:26 +0300

    make edit in Validator class. check for () and for other illegal symbols

--- scr/com/zufrost/learn/service/Validator.java
+++ scr/com/zufrost/learn/service/Validator.java
@@ -9,12 +9,17 @@ public class Validator implements IValidator {
         if (stringExpression.equals("")) {
             throw new IllegalArgumentException("You input empty arithmetical expression");
         }
-
-
+        // проверка на ()
+        if (stringExpression.contains("(") || stringExpression.contains(")")) {
+            throw new IllegalArgumentException("Данная версия программы не поддерживает скобки \"()\"");
+        }
         // проверка на правильность ввода
-//        if (!stringExpression.matches("[\\d0-9+\\-*\\/.]")) {
-//            System.out.println("строка содержит запрещенные символы");
-//        }
+        if (!stringExpression.matches("[\\d0-9+\\-*\\/.]")) {
+            throw new IllegalArgumentException("Строка содержит запрещенные символы. Используйте только цифры 0-9 разделитель дроби '.' " +
+                    "и знаки математических операций '+' '-' '*' '/' ");
+//            System.out.println("Строка содержит запрещенные символы. Используйте только цифры 0-9 разделитель дроби '.' " +
+//                    "и знаки математических операций '+' '-' '*' '/' ");
+        }
         // вывод исключений и их обработка
         // возврат полученной строки в нераспарсенном виде в соответствующее поле
 
